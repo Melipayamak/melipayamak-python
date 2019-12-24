bdbf75187a8041879b2b07c9e0a9988159655513
ملی پیامک <melipayamak.ir@gmail.com>
2019-12-24 15:41:05 +0330

    Add files via upload

--- melipayamak/melipayamak.py
+++ melipayamak/melipayamak.py
@@ -1,8 +1,12 @@
-from .sms import Rest, Soap
+from .sms import Rest, Soap, RestAsync, SoapAsync
 from .branch import Branch
+from .branchAsync import BranchAsync
 from .users import Users
+from .usersAsync import UsersAsync
 from .ticket import Ticket
+from .ticketAsync import TicketAsync
 from .contacts import Contacts
+from .contactsAsync import ContactsAsync
 
 
 class Api:
@@ -10,20 +14,38 @@ class Api:
         self.username = username
         self.password = password
 
-    def sms(self, _type="rest"):
-        if _type == "rest":
-            return Rest(self.username, self.password)
+    def sms(self, _method="rest", _type=""):
+        if _method == "rest":
+            if _type == "async":
+                return RestAsync(self.username, self.password)
+            else:
+                return Rest(self.username, self.password)
         else:
-            return Soap(self.username, self.password)
+            if _type == "async":
+                return SoapAsync(self.username, self.password)
+            else:
+                return Soap(self.username, self.password)
 
     def users(self):
         return Users(self.username, self.password)
 
+    def usersAsync(self):
+        return UsersAsync(self.username, self.password)
+
     def ticket(self):
         return Ticket(self.username, self.password)
+
+    def ticketAsync(self):
+        return TicketAsync(self.username, self.password)
 
     def branch(self):
         return Branch(self.username, self.password)
 
+    def branchAsync(self):
+        return BranchAsync(self.username, self.password)
+
     def contacts(self):
         return Contacts(self.username, self.password)
+
+    def contactsAsync(self):
+        return ContactsAsync(self.username, self.password)
