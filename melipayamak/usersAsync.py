32d9c7278b61d399fed9c4bde0ae82409444f4a8
Omid Adib <omid.adibfar@yahoo.com>
2021-04-19 19:53:02 +0430

    Update usersAsync.py

--- melipayamak/usersAsync.py
+++ melipayamak/usersAsync.py
@@ -1,6 +1,6 @@
 import zeep
-from zeep.asyncio import AsyncTransport
 import asyncio
+from zeep.transports import AsyncTransport
 
 
 class UsersAsync:
@@ -9,7 +9,6 @@ class UsersAsync:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        
 
     def get_data(self):
         return {
@@ -41,22 +40,17 @@ class UsersAsync:
         # print("time: %.2f" % (time.time() - st))
         return result
 
-
     def add_payment(self, options):
         return self.makeRequest('AddPayment', {**self.get_data(), **options})
-        
 
     def add(self, options):
         return self.makeRequest('AddUser', {**self.get_data(), **options})
 
-
     def add_complete(self, options):
         return self.makeRequest('AddUserComplete', {**self.get_data(), **options})
-        
 
     def add_with_location(self, options):
         return self.makeRequest('AddUserWithLocation', {**self.get_data(), **options})
-        
 
     def authenticate(self):
         return self.makeRequest('AuthenticateUser', self.get_data())
@@ -70,7 +64,6 @@ class UsersAsync:
         }
         return self.makeRequest('ChangeUserCredit', {**self.get_data(), **data})
 
-
     def forgot_password(self, mobileNumber, emailAddress, targetUsername):
         data = {
             'mobileNumber': mobileNumber,
@@ -79,13 +72,11 @@ class UsersAsync:
         }
         return self.makeRequest('ForgotPassword', {**self.get_data(), **data})
 
-
     def get_base_price(self, targetUsername):
         data = {
             'targetUsername': targetUsername
         }
         return self.makeRequest('GetUserBasePrice', {**self.get_data(), **data})
-
 
     def remove(self, targetUsername):
         data = {
@@ -93,13 +84,11 @@ class UsersAsync:
         }
         return self.makeRequest('RemoveUser', {**self.get_data(), **data})
 
-
     def get_credit(self, targetUsername):
         data = {
             'targetUsername': targetUsername
         }
         return self.makeRequest('GetUserCredit', {**self.get_data(), **data})
-
 
     def get_details(self, targetUsername):
         data = {
@@ -107,14 +96,11 @@ class UsersAsync:
         }
         return self.makeRequest('GetUserDetails', {**self.get_data(), **data})
 
-
     def get_numbers(self):
         return self.makeRequest('GetUserNumbers', self.get_data())
-        
 
     def get_provinces(self):
         return self.makeRequest('GetProvinces', self.get_data())
-        
 
     def get_cities(self, provinceId):
         data = {
@@ -122,10 +108,8 @@ class UsersAsync:
         }
         return self.makeRequest('GetCities', {**self.get_data(), **data})
 
-
     def get_expire_date(self):
         return self.makeRequest('GetExpireDate', self.get_data())
-        
 
     def get_transactions(self, targetUsername, creditType, dateFrom, dateTo, keyword):
         data = {
@@ -136,11 +120,9 @@ class UsersAsync:
             'keyword': keyword
         }
         return self.makeRequest('GetUserTransactions', {**self.get_data(), **data})
-        
 
     def get(self):
         return self.makeRequest('GetUsers', self.get_data())
-        
 
     def has_filter(self, text):
         data = {
